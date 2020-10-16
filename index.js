a8b1fd084d28c3173a2bf520491327798add6254
BenLooper <benlooper08@gmail.com>
2020-10-15 19:59:19 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => sentence.split(' ').map(word => word.replace(word[0], word[0].toUpperCase())).join(' '))
 }
